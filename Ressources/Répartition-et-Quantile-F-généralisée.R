022359b1929f9ba92d4ab2c29e08f7c9b64d6148
Alec James van Rassel <alec.van-rassel.1@ulaval.ca>
2019-06-22 16:35:47 -0400

    Ajout code de Marceau pour F-Gen

---
+++ Ressources/Répartition-et-Quantile-F-généralisée.R
@@ -0,0 +1,26 @@
+#####Fonction de répartition, Quantile, et TVaRX d'une F-généralisée
+### Marceau
+
+kappa <- 0.995
+lambda <- (44000 / 43)
+alpha <- (87 / 43)
+tho <- 2
+
+pFgen <- function(x, lambda, alpha, tho)
+{
+    pbeta(q = (x / (lambda + x)), 
+          shape1 = tho, 
+          shape2 = alpha)
+}
+
+qFgen <- function(x) {
+    abs(pFgen(x, lambda, alpha, tho) - kappa)
+}
+
+qFgenTest <- function(x, kappa)
+{
+    if(kappa <= 0)
+        0
+    else
+        optimize(qFgen, c(0, 30000))$minimum
+}
